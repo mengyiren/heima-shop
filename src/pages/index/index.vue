22a5b3f9a1ff47f616eb14317ceb4561afd353fc
mengyiren <329274258@qq.com>
2025-04-14 10:59:21 +0800

    热门推荐

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { http } from '@/utils/http'
 import CustomNavbar from './components/CustomNavbar.vue'
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
-import type { BannerItem, CategoryItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import CategoryPanel from '@/components/CategoryPanel.vue'
-
+import HotPanel from './components/HotPanel.vue'
 //
 const bannerList = ref<BannerItem[]>([])
 const getBannerData = async () => {
@@ -20,9 +20,16 @@ const getCategoryData = async () => {
   categoryList.value = res.result
 }
 
+const hotList = ref<HotItem[]>([])
+const getHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
 onLoad(() => {
   getBannerData()
   getCategoryData()
+  getHotData()
 })
 </script>
 
@@ -30,6 +37,7 @@ onLoad(() => {
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
   <CategoryPanel :list="categoryList" />
+  <HotPanel :list="hotList" />
   <view class="index">index</view>
 </template>
 
--- src/types/home.d.ts
+++ src/types/home.d.ts
@@ -15,3 +15,18 @@ export type CategoryItem = {
   id: string
   name: string
 }
+
+export type HotItem = {
+  /** 说明 */
+  alt: string
+  /** id */
+  id: string
+  /** 图片集合[ 图片路径 ] */
+  pictures: string[]
+  /** 跳转地址 */
+  target: string
+  /** 标题 */
+  title: string
+  /** 推荐类型 */
+  type: string
+}
